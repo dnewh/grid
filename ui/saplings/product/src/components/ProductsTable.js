a626de43247f060bd93a18dc470be9038e68faec
Davey Newhall <newhall@bitwise.io>
2021-04-21 11:55:15 -0500

    Update ProductsTable component
    This updates the ProductsTable component per the updated designs. This
    uses the other new/updated components. This page now contains a table
    that displays products for a chosen service. This table is filterable
    and product xml files can be downloaded directly from the table.
    
    Signed-off-by: Davey Newhall <newhall@bitwise.io>

--- ui/saplings/product/src/components/ProductsTable.js
+++ ui/saplings/product/src/components/ProductsTable.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018-2020 Cargill Incorporated
+ * Copyright 2018-2021 Cargill Incorporated
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,88 +14,237 @@
  * limitations under the License.
  */
 
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import React, { useState, useMemo, useEffect } from 'react';
+import Icon from '@material-ui/core/Icon';
+import xml2js from 'xml2js';
+import { matchSorter } from 'match-sorter';
+import _ from 'lodash';
+import { Chip, Chips } from './Chips';
+import { Table } from './Table';
 import { useServiceState } from '../state/service-context';
-import ProductCard from './ProductCard';
-import NotFound from './NotFound';
-import Loading from './Loading';
-import { getProperty } from '../data/property-parsing';
+import { Input } from './Input';
 import { listProducts } from '../api/grid';
 import './ProductsTable.scss';
 
-function ProductsTable({ actions }) {
+function ProductsTable() {
   const [products, setProducts] = useState([]);
   const { selectedService } = useServiceState();
-  const [loading, setLoading] = useState(false);
+  const [filterInputState, setFilterInputState] = useState({
+    type: 'product name',
+    value: ''
+  });
+  const initialFilterState = [
+    {
+      type:'product name',
+      value: ''
+    },
+    {
+      type: 'gtin',
+      value: ''
+    },
+    {
+      type: 'owner',
+      value: ''
+    }
+  ]
+  const [filterState, setFilterState] = useState(initialFilterState);
+
+  const productXMLToJSON = product => {
+    let parser = new xml2js.Parser({
+      explicitArray: false,
+      strict: false,
+    });
+    parser = parser.parseString;
+    const productJSON = [];
+    const xmlProperty = _.find(product.properties, ['name', 'xml_data']).string_value;
+    parser(xmlProperty, (err, result) => {
+      if (err) {
+        console.error(err);
+      } else {
+        productJSON.push(result);
+      }
+    });
+    return productJSON;
+  }
 
   useEffect(() => {
     const getProducts = async () => {
       if (selectedService !== 'none') {
-        setLoading(true);
         try {
           const productList = await listProducts(selectedService);
-          setProducts(productList);
+          setProducts(productList.map(p => ({
+            product: p,
+            data: productXMLToJSON(p)
+          })));
         } catch (e) {
           console.error(`Error listing products: ${e}`);
         }
       } else {
         setProducts([]);
       }
-      setLoading(false);
     };
 
     getProducts();
   }, [selectedService]);
 
-  const productCards = products.map(product => {
-    return (
-      <ProductCard
-        key={product.product_id}
-        gtin={product.product_id}
-        service={selectedService}
-        name={getProperty('product_name', product.properties)}
-        owner={product.orgName}
-        imageURL={getProperty('image_url', product.properties)}
-        editFn={actions.editProduct}
-        deleteFn={actions.deleteProduct}
-        properties={product.properties}
-      />
-    );
-  });
-
-  const getContent = () => {
-    if (loading) {
-      return <Loading />;
+  const handleFilterInputChange = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'filter-type':
+        setFilterInputState({
+          type: value,
+          value: filterInputState.value
+        });
+        break;
+      case 'filter-value':
+        setFilterInputState({
+          type: filterInputState.type,
+          value
+        });
+        break;
+      default:
+        break;
     }
-    if (products.length === 0) {
-      return <NotFound message="No Products" />;
-    }
-    return <div className="products-table">{productCards}</div>;
   };
+
+  const handleAddFilter = () => {
+      filterState.find(f => f.type === filterInputState.type).value = filterInputState.value;
+      setFilterState([
+        ...filterState
+      ])
+
+      setFilterInputState({
+        type: filterInputState.type,
+        value: ''
+      })
+  }
+
+  const handleRemoveFilter = c => {
+    filterState.find(f => f.type === c).value = '';
+      setFilterState([
+        ...filterState
+      ])
+  }
+
+  const fuzzyTextFilter = (rows, id, filterValue) => {
+    return matchSorter(rows, filterValue, { keys: [row => row.values[id]] });
+  }
+
+  const accessProductImage = row => {
+    const ns = row.data[0].GRIDTRADEITEMS.TRADEITEM.TRADEITEMINFORMATION.EXTENSION["NS8:REFERENCEDFILEDETAILINFORMATIONMODULE"];
+    if (ns) {
+      const img = _.find(ns, (r) => r.REFERENCEDFILETYPECODE === "PRODUCT_IMAGE")
+      if (img) {
+        return img.UNIFORMRESOURCEIDENTIFIER;
+      }
+    }
+    return undefined;
+  }
+
+  const accessProductName = row => {
+    if (row.data[0].GRIDTRADEITEMS.TRADEITEM.TRADEITEMINFORMATION.EXTENSION["NS9:TRADEITEMDESCRIPTIONMODULE"]) {
+      return row.data[0].GRIDTRADEITEMS.TRADEITEM.TRADEITEMINFORMATION.EXTENSION["NS9:TRADEITEMDESCRIPTIONMODULE"].TRADEITEMDESCRIPTIONINFORMATION.REGULATEDPRODUCTNAME._;
+    }
+    return undefined;
+  }
+
+  const filterTypes = useMemo(
+    () => ({
+      fuzzyText: fuzzyTextFilter,
+    })
+  )
+
+  const filters = useMemo(
+    () => filterState
+  )
+
+  const data = useMemo(
+    () => products
+  )
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'GTIN',
+        accessor: 'data[0].GRIDTRADEITEMS.TRADEITEM.GTIN',
+        id: 'gtin',
+        filter: 'fuzzyText'
+      },
+      {
+        Header: 'Image',
+        accessor: accessProductImage,
+        id: 'image',
+        // eslint-disable-next-line react/prop-types, react/destructuring-assignment
+        Cell: props => <img src={props.value} alt={`${props.row.values.gtin} thumbnail`} className="product-image" />
+      },
+      {
+        Header: 'Product Name',
+        accessor: accessProductName,
+        id: 'product name',
+        // eslint-disable-next-line react/prop-types, react/destructuring-assignment
+        Cell: props => <a href={`/product/${props.row.values.gtin}`}>{props.value}</a>,
+        filter: 'fuzzyText'
+      },
+      {
+        Header: 'Created',
+        accessor: 'product.created',
+      },
+      {
+        Header: 'Circuit Name/ID',
+        accessor: 'product.service_id',
+      },
+      {
+        Header: 'Owner',
+        accessor: 'product.orgName',
+        filter: 'fuzzyText',
+        id: 'owner'
+      },
+    ]
+  )
 
   return (
     <div className="products-table-container">
-      <div className="products-table-header">
-        <h5 className="title">Products</h5>
-        <hr />
+      <h1 className="selected-service">{selectedService === 'none' ? 'No service selected' : selectedService}</h1>
+      <div className="table-utils">
+        <div className="util-wrapper">
+          <div className="filters">
+            <div className="inputs">
+              <Input type="select" icon="filter_list" label="Filter By" name="filter-type" value={filterInputState.type} onChange={handleFilterInputChange}>
+                <option value="product name" default>
+                  Product name
+                </option>
+                <option value="gtin">GTIN</option>
+                <option value="owner">Owner</option>
+              </Input>
+              <Input type="text" icon="search" label="Product Name" name="filter-value" value={filterInputState.value} onChange={handleFilterInputChange} />
+              <button className="btn-primary" onClick={handleAddFilter} type="button">Add Filter</button>
+            </div>
+            <div className="filter-list">
+              {filterState.some(f => f.value !== '') &&
+                <>
+                  <Chips>
+                    {
+                      filterState.map(({type, value}) => (value !== '' && <Chip deleteable key={type} label={`${type}: ${value}`} removeFn={() => handleRemoveFilter(type)} />))
+                    }
+                  </Chips>
+                  <button className="btn-primary btn-min" onClick={() => setFilterState(initialFilterState)} type="button">Clear Filters</button>
+                </>
+              }
+            </div>
+          </div>
+          <div className="actions">
+            <button className="btn-primary" type="button">
+              <Icon>add</Icon>
+              <span>Add</span>
+            </button>
+          </div>
+        </div>
       </div>
-      {getContent()}
-      <button
-        className="fab add-product"
-        type="button"
-        onClick={actions.addProduct}
-      >
-        <FontAwesomeIcon icon="plus" />
-      </button>
+      <div className="table">
+        <Table columns={columns} data={data} filterTypes={filterTypes} filters={filters} />
+      </div>
     </div>
   );
 }
-
-ProductsTable.propTypes = {
-  actions: PropTypes.object.isRequired
-};
 
 export default ProductsTable;
